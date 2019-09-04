09f3fe5fe9abde2f65796920f6dd99c55ffd6bb1
CoryGreen <43450415+CoryGreen@users.noreply.github.com>
2019-09-04 16:31:38 +0100

    Update burgerBuilder.js

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actionTypes';
+eimport * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
 export const addIngredient = ( name ) => {
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get( 'https://react-burger-builder-68b2f.firebaseio.com/ingredients.json' )
+        axios.get( '<deleted>' )
             .then( response => {
                dispatch(setIngredients(response.data));
             } )
